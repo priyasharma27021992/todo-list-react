26d2812988e832e6b8c93000d899c405fe35fa96
Priya.Sharma <priyasharma2702@gmail.com>
2024-10-17 22:47:05 +0530

    making css better

--- src/components/Task.tsx
+++ src/components/Task.tsx
@@ -13,61 +13,62 @@ export const Task = ({ task, onChange, onDelete }: TaskProps) => {
   let taskContent;
   if (isEditing) {
     taskContent = (
-      <>
-        <fieldset>
-          <label htmlFor="taskDescription" hidden>
-            TaskDescription
-          </label>
-          <input
-            type="text"
-            className="task-text"
-            name="taskDescription"
-            autoFocus
-            value={task.text}
-            onChange={(e) => onChange({ ...task, text: e.target.value })}
-          />
-        </fieldset>
-        <button
-          type="button"
-          onClick={() => {
-            setIsEditing(false);
-          }}
-        >
-          Save
-        </button>
-      </>
+      <fieldset>
+        <label htmlFor="taskDescription" hidden>
+          TaskDescription
+        </label>
+        <input
+          type="text"
+          className="task-text"
+          name="taskDescription"
+          autoFocus
+          value={task.text}
+          onChange={(e) => onChange({ ...task, text: e.target.value })}
+        />
+      </fieldset>
     );
   } else {
-    taskContent = (
-      <>
-        <div className="task-text">{task.text}</div>
-        <button
-          type="button"
-          onClick={() => {
-            setIsEditing(true);
-          }}
-        >
-          Edit
-        </button>
-      </>
-    );
+    taskContent = <div className="task-text">{task.text}</div>;
   }
 
   return (
     <>
-      <label htmlFor="status" hidden>
-        Status
-      </label>
-      <input
-        type="checkbox"
-        name="status"
-        checked={task.done}
-        onChange={(e) => onChange({ ...task, done: e.target.checked })}
-      />
-      <div className="task-content">{taskContent}</div>
-      <button type="button" onClick={() => onDelete(task.id)}>
-        Delete
-      </button>
+      <div className="task-input">
+        <label htmlFor="status" hidden>
+          Status
+        </label>
+        <input
+          type="checkbox"
+          name="status"
+          checked={task.done}
+          onChange={(e) => onChange({ ...task, done: e.target.checked })}
+        />
+        <div className="task-content">{taskContent}</div>
+      </div>
+      <div>
+        {isEditing ? (
+          <button
+            type="button"
+            onClick={() => {
+              setIsEditing(false);
+            }}
+          >
+            Save
+          </button>
+        ) : (
+          <button
+            type="button"
+            onClick={() => {
+              setIsEditing(true);
+            }}
+          >
+            Edit
+          </button>
+        )}
+        <button type="button" onClick={() => onDelete(task.id)}>
+          Delete
+        </button>
+      </div>
     </>
   );
 };
